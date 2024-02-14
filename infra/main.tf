fe726f66ff340a43ded44ef0777198a7d2942d3f
Divya Shree <shree.d@northeastern.edu>
2024-02-14 14:15:37 -0500

    Multiple workflows code and gitbub workflow

--- infra/main.tf
+++ infra/main.tf
@@ -1,29 +1,36 @@
 # Purpose: Create a VPC for the project
-resource "google_compute_network" "cloud-assignment-vpc" {
-  name = "cloud-assignment-vpc"
+resource "google_compute_network" "vpc_name" {
+  for_each = {
+    for index, name in var.vpc_name : name => index
+  }
+  name = each.key
   auto_create_subnetworks = false
+  delete_default_routes_on_create = true
 }
 
 # Purpose: Create a webapp subnet for the VPC
 resource "google_compute_subnetwork" "webapp" {
-  name          = "webapp"
+  for_each = google_compute_network.vpc_name
+  name          = "${each.key}-webapp"
   ip_cidr_range = var.webapp_ip_cidr_range
   region        = var.region
-  network       = google_compute_network.cloud-assignment-vpc.self_link
+  network       = each.value.self_link
 }
 
 # Purpose: Create a db subnet for the VPC
 resource "google_compute_subnetwork" "db" {
-  name          = "db"
+  for_each = google_compute_network.vpc_name
+  name          = "${each.key}-db"
   ip_cidr_range = var.db_ip_cidr_range
   region        = var.region
-  network       = google_compute_network.cloud-assignment-vpc.self_link
+  network       = each.value.self_link
 }
 
 # Define routes
 resource "google_compute_route" "webapp_route" {
-    name                  = "webapp-route"
-    network               = google_compute_network.cloud-assignment-vpc.self_link
+    for_each = google_compute_network.vpc_name
+    name                  = "${each.key}-webapp-route"
+    network               = each.value.self_link
     dest_range            = var.webapp_destination_ip_range
     next_hop_gateway      = var.webapp_route_next_hop_gateway
 }
